a15800cf2526f8ae56aba4fc523d378e5c021dd0
Ajit Dubey <ajitdubey.it@gmail.com>
2020-06-28 13:07:40 +0530

    dfds

--- SpringSecurityLDAP/src/main/java/com/example/SpringSecurityLDAP/HomeResource.java
+++ SpringSecurityLDAP/src/main/java/com/example/SpringSecurityLDAP/HomeResource.java
@@ -3,6 +3,9 @@ package com.example.SpringSecurityLDAP;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//username : ben
+//Password : benspassword
+// https://spring.io/guides/gs/authenticating-ldap/   
 @RestController
 public class HomeResource {
 
